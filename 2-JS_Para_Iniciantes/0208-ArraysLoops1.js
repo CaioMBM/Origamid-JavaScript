c0d416edde496543f025d88784b697c39c8cbd2c
Caio Montenegro <caiombmonteiro@gmail.com>
2025-04-02 15:44:03 -0300

    0208-ArraysLoops1
    Assuntos abordados:
    - Array;
    - Loop: for e while.

---
+++ 2-JS_Para_Iniciantes/0208-ArraysLoops1.js
@@ -0,0 +1,29 @@
+// Array 
+/* 
+var videoGames = ['Switch', 'PS4', 'XBox'];
+
+videoGames[0] // Switch
+videoGames[2] // Xbox
+
+videoGames.pop();       // Remove o último item e retorna ele
+videoGames.push('3DS'); // Adiciona ao final da array
+videoGames.length;      // 3
+*/
+
+// Loop - For
+/*
+for (var numero = 0; numero < 10; numero++) {
+    console.log(numero); 
+}
+// Retorna de 0 a 9 no console
+*/
+
+// Loop - While
+/* 
+var i = 0;          // Início
+while (i < 10) {    // Condição
+console.log(i);   
+i++;              // Incremento para incrementar mais 1 ao novo valor
+}
+*/
+// Retorna de 0 a 9 no console
