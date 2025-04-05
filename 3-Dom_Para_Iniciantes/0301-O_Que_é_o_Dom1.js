16f5a182c4dc684c036e0476c0747d7557d2fb48
Caio Montenegro <caiombmonteiro@gmail.com>
2025-04-05 16:38:07 -0300

    0301 - O Que é o Dom 1
    Assuntos abordados:
    - DOM;
    - Hierarquia do DOM;
    - Acessando um elemento HTML com DOM.

---
+++ 3-Dom_Para_Iniciantes/0301-O_Que_é_o_Dom1.js
@@ -0,0 +1,15 @@
+// Retorna um alerta com a mensagem que está dentro dos parênteses assim que a página é carregada ou recarregada
+/*
+window.alert("Alerta!!");
+*/
+
+// Retorna a url da página
+/*
+const linkPagina = window.location.href;
+console.log(linkPagina);
+*/
+
+// Selecionando um elemento HTML (uma das formas)
+/*
+const h1 = document.querySelector('h1');
+*/
