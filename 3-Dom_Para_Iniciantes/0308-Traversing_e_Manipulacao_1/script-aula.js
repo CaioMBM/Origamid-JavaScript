f135e569475b0944c5140fb2a11420366649d535
Caio Montenegro <caiombmonteiro@gmail.com>
2025-08-12 19:22:04 -0300

    0308-Traversing e Manipulação 2
    Assuntos abordados:
    - .appendChild() para adicionar um elemento no final da fila;
    - .insertBefore() para inserir um elemento no lugar de outro;
    - .removeChild() para remover um elemento;
    - .replaceChild() para substituir um elemento;
    - .createElement() para criar um elemento;
    - .cloneNode(true) para clonar um elemento por completo.

--- 3-Dom_Para_Iniciantes/0308-Traversing_e_Manipulacao_1/script-aula.js
+++ 3-Dom_Para_Iniciantes/0308-Traversing_e_Manipulacao_1/script-aula.js
@@ -46,3 +46,37 @@ console.log(lista.firstChild);             // Saída: #text
 // Retorna o todos os node child
 console.log(lista.childNodes);             // Saída: NodeList(13) [text, li, text, li, text, li, text, li, text, li, text, li, text]
 */
+
+// Manipulando Elementos - appendChild(), insertBefore(), removeChild() e replaceChild()
+/*
+const lista = document.querySelector('.animais-lista');
+const contato = document.querySelector('.contato');
+const titulo = contato.querySelector('.titulo');
+
+contato.appendChild(lista); // move lista para o final de contato
+contato.insertBefore(lista, titulo); // insere a lista antes de titulo
+contato.removeChild(titulo); // remove titulo de contato
+contato.replaceChild(lista, titulo); // substitui titulo por lista
+*/
+
+// Novos Elementos - .createElement()
+/*
+const animais = document.querySelector('.animais');
+
+const novoH1 = document.createElement('h1');
+novoH1.innerText = 'Novo Título';
+novoH1.classList.add('titulo');
+
+animais.appendChild(novoH1);
+*/
+
+// Clonar Elementos
+/*
+// Clonei titulo
+const titulo      = document.querySelector('.titulo')
+const cloneTitulo = titulo.cloneNode(true);
+
+// Adicionei no fim de contato
+const contato = document.querySelector('.contato');
+contato.appendChild(cloneTitulo);
+*/
--- 3-Dom_Para_Iniciantes/0308-Traversing_e_Manipulacao_1/script.js
+++ 3-Dom_Para_Iniciantes/0308-Traversing_e_Manipulacao_1/script.js
@@ -1,1 +1,29 @@
 // Exercício
+// Duplique o menu e adicione ele em copy
+/*
+const menu          = document.querySelector('.menu');
+const menuDuplicado = menu.cloneNode(true);
+
+const copy = document.querySelector('.copy');
+copy.appendChild(menuDuplicado);
+*/
+
+// Selecione o primeiro DT da dl de Faq
+/*
+const primeraDT = document.querySelectorAll('dt')[0];
+*/
+
+// Selecione o DD referente ao primeiro DT
+/*
+const primeraDD = document.querySelectorAll('dd')[0];
+console.log(primeraDD);
+// Obs: A resolução do professor foi diferente.
+*/
+
+// Substitua o conteúdo html de .faq pelo de .animais
+/*
+const faq = document.querySelector('.faq');
+const animais = document.querySelector('.animais');
+
+faq.innerHTML = animais.innerHTML;
+*/
