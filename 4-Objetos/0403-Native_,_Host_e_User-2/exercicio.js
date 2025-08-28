0e20861970e1d1abaeaa1b063d302827a1f83c76
Caio Montenegro <caiombmonteiro@gmail.com>
2025-08-28 04:10:27 -0300

    0403 - Native, Host e User 2
    - Exercício.

---
+++ 4-Objetos/0403-Native_,_Host_e_User-2/exercicio.js
@@ -0,0 +1,14 @@
+// Liste 5 objetos nativos
+/*
+Math, String, Array, Function e Date. 
+*/
+
+// Liste 5 objetos do browser
+/*
+Window, history, Document, HTMLAllCollection e NodeList.
+*/
+
+// Liste 2 Métodos, Propriedades ou Objetos presentes no Chrome que não existem no Firefox
+/*
+onafterprint e adoptNode.
+*/
